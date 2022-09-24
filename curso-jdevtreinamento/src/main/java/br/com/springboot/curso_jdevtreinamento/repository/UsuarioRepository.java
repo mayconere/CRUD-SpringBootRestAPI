eceb9820884a706ce04dd554a44e4ac99c26ceb8
mayconere <maycon.silva42@live.com>
2022-09-23 21:51:39 -0300

    Criando END-POINT para consultar por parte do nome

--- curso-jdevtreinamento/src/main/java/br/com/springboot/curso_jdevtreinamento/repository/UsuarioRepository.java
+++ curso-jdevtreinamento/src/main/java/br/com/springboot/curso_jdevtreinamento/repository/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package br.com.springboot.curso_jdevtreinamento.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.springboot.curso_jdev_treinamento.model.Usuario;
@@ -8,4 +11,6 @@ import br.com.springboot.curso_jdev_treinamento.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
+	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")
+	List<Usuario> buscarPorNome(String name);
 }
